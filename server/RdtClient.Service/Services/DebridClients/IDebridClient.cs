c35540c8c75ef90419a6d58fa7f0bfb19b31da85
omgbeez <251408589+omgbeez@users.noreply.github.com>
2026-02-13 14:31:25 -0500

    Rebase on usenet changes

--- server/RdtClient.Service/Services/DebridClients/IDebridClient.cs
+++ server/RdtClient.Service/Services/DebridClients/IDebridClient.cs
@@ -13,10 +13,10 @@ public interface IDebridClient
     Task<String> AddNzbFile(Byte[] bytes, String? name);
     Task<IList<DebridClientAvailableFile>> GetAvailableFiles(String hash);
     /// <summary>
-    ///     Tell the debrid provider which files to download.
+    /// Tell the debrid provider which files to download.
     /// </summary>
     /// <remark>
-    ///     Not all providers support this feature.
+    /// Not all providers support this feature.
     /// </remark>
     /// <param name="torrent">The torrent to select files for</param>
     /// <returns>Number of files selected</returns>
@@ -25,11 +25,9 @@ public interface IDebridClient
     Task<String> Unrestrict(Torrent torrent, String link);
     Task<Torrent> UpdateData(Torrent torrent, DebridClientTorrent? torrentClientTorrent);
     Task<IList<DownloadInfo>?> GetDownloadInfos(Torrent torrent);
-
     /// <summary>
-    ///     To be called only when <see cref="Data.Models.Data.Download" />.<see cref="Data.Models.Data.Download.FileName" />
-    ///     is not set by
-    ///     <see cref="GetDownloadInfos" />
+    /// To be called only when <see cref="Data.Models.Data.Download" />.<see cref="Data.Models.Data.Download.FileName" /> is not set by
+    /// <see cref="GetDownloadInfos" />
     /// </summary>
     /// <param name="download">The download to get the filename of</param>
     /// <returns>The filename of the download</returns>
